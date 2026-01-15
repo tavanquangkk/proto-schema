8842ef60b8a96868c3e7b6366fad59bc7cfc5e7f
taquang <taquanga6dp@gmail.com>
2026-01-15 22:56:08 +0900

    aa

--- scripts/gen_dart.sh
+++ scripts/gen_dart.sh
@@ -5,17 +5,33 @@ OUT=out/dart
 rm -rf $OUT
 mkdir -p $OUT
 
+echo "--- Generating Dart code from Protos ---"
+
+# Chạy lệnh protoc
+# Đảm bảo các thư mục proto/master và proto/slip tồn tại
 protoc \
   -I proto \
   --dart_out=grpc:$OUT \
   proto/master/*.proto \
   proto/slip/*.proto
 
+echo "--- Creating Barrel files (exports) ---"
+
+# Tạo các file export để import dễ dàng hơn
 for dir in $OUT/*; do
-  [ -d "$dir" ] || continue
-  name=$(basename "$dir")
-  echo "// auto-generated" > $OUT/$name.dart
-  for f in $dir/*.pb.dart; do
-    echo "export '$name/$(basename "$f")';" >> $OUT/$name.dart
-  done
+  if [ -d "$dir" ]; then
+    name=$(basename "$dir")
+    export_file="$OUT/$name.dart"
+    
+    echo "// auto-generated" > "$export_file"
+    for f in "$dir"/*.pb.dart; do
+      if [ -f "$f" ]; then
+        filename=$(basename "$f")
+        echo "export '$name/$filename';" >> "$export_file"
+      fi
+    done
+    echo "Created $export_file"
+  fi
 done
+
+echo "--- Done ---"
